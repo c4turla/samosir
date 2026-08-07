a7eef6fd6f4dbc802b39e84cef0e0de4de75e1c3
c4turla <catur.crh@gmail.com>
2026-08-07 14:35:26 +0700

    Fix sed pattern to match leading whitespace in .env lines

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ -f /var/www/html/.env ]; then
-    sed -i "s|database.default.hostname = .*|database.default.hostname = ${DB_HOST:-db}|" /var/www/html/.env
-    sed -i "s|database.default.database = .*|database.default.database = ${DB_DATABASE:-samosir}|" /var/www/html/.env
-    sed -i "s|database.default.username = .*|database.default.username = ${DB_USERNAME:-samosir}|" /var/www/html/.env
-    sed -i "s|database.default.password = .*|database.default.password = ${DB_PASSWORD}|" /var/www/html/.env
+    sed -i "s|^[[:space:]]*database.default.hostname = .*|database.default.hostname = ${DB_HOST:-db}|" /var/www/html/.env
+    sed -i "s|^[[:space:]]*database.default.database = .*|database.default.database = ${DB_DATABASE:-samosir}|" /var/www/html/.env
+    sed -i "s|^[[:space:]]*database.default.username = .*|database.default.username = ${DB_USERNAME:-samosir}|" /var/www/html/.env
+    sed -i "s|^[[:space:]]*database.default.password = .*|database.default.password = ${DB_PASSWORD}|" /var/www/html/.env
     if [ -n "${APP_URL}" ]; then
         if grep -q "^[[:space:]]*app.baseURL" /var/www/html/.env; then
             sed -i "s|^[[:space:]]*app.baseURL = .*|app.baseURL = '${APP_URL}'|" /var/www/html/.env
